edcf8b53b23858b1b532945dcd1d72077146e9b4
bseony2 <bseony2@gmail.com>
2023-09-14 00:27:46 +0900

    refact : 게임종료에 대한 사용자의 입력값을 GameStatus에 메시지로 전달하도록 수정

--- src/main/java/baseball/enums/GameStatus.java
+++ src/main/java/baseball/enums/GameStatus.java
@@ -9,6 +9,11 @@ public enum GameStatus {
     GameStatus(int value) {
         this.value = value;
     }
+
+    public static boolean isUserWnatNewGame(int status) {
+        return status == NEW.getValue();
+    }
+
     public int getValue() {
         return this.value;
     }
